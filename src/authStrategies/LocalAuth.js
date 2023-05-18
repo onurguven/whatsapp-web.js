6b362e97f51638b3d1f35e35f5fe79521862ff3d
onurguven <onurrguven@gmail.com>
2023-05-18 22:27:04 +0300

    refactor(LocalAuth) improve logout() LocalAuth logic

--- src/authStrategies/LocalAuth.js
+++ src/authStrategies/LocalAuth.js
@@ -8,7 +8,7 @@ const BaseAuthStrategy = require('./BaseAuthStrategy');
  * Local directory-based authentication
  * @param {object} options - options
  * @param {string} options.clientId - Client id to distinguish instances if you are using multiple, otherwise keep null if you are using only one instance
- * @param {string} options.dataPath - Change the default path for saving session files, default is: "./.wwebjs_auth/" 
+ * @param {string} options.dataPath - Change the default path for saving session files, default is: "./.wwebjs_auth/"
 */
 class LocalAuth extends BaseAuthStrategy {
     constructor({ clientId, dataPath }={}) {
@@ -33,7 +33,7 @@ class LocalAuth extends BaseAuthStrategy {
         }
 
         fs.mkdirSync(dirPath, { recursive: true });
-        
+
         this.client.options.puppeteer = {
             ...puppeteerOpts,
             userDataDir: dirPath
@@ -44,7 +44,8 @@ class LocalAuth extends BaseAuthStrategy {
 
     async logout() {
         if (this.userDataDir) {
-            return (fs.rmSync ? fs.rmSync : fs.rmdirSync).call(this, this.userDataDir, { recursive: true });
+            await this.client.pupBrowser.close();
+            return (fs.rmSync ? fs.rmSync : fs.rmdirSync).call(this, this.userDataDir, { recursive: true, force: true });
         }
     }
 
